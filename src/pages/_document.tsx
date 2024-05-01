bce21299ac54f48e7d156eee41bce1e2f288e789
galib6 <asadullahalgalib81@gmail.com>
2024-05-01 12:15:41 +0600

    css chaching updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,18 @@
-import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
-import { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import type { DocumentContext } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicons/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const MyDocument = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
-Document.getInitialProps = async (ctx: DocumentContext) => {
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   const cache = createCache();
   const originalRenderPage = ctx.renderPage;
   ctx.renderPage = () =>
@@ -43,3 +36,5 @@ Document.getInitialProps = async (ctx: DocumentContext) => {
     ),
   };
 };
+
+export default MyDocument;
